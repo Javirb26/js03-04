eab7c0ddee8e2014572307fb93803296f2f5d095
Javirb26 <112392832+Javirb26@users.noreply.github.com>
2023-02-03 11:39:46 -0500

    Add files via upload
    JavaScript Web for Warriors: Chapter 3: Hands on Project 4 - Customer Reviews of Dance Off

---
+++ project03-04_txt.js
@@ -0,0 +1,23 @@
+/*    JavaScript 7th Edition
+      Chapter 3
+      Project 03-04
+
+      Application to write a list of customer reviews
+      Author: Javier Ballester
+      Date: 02/03/2023  
+
+      Filename: project03-04.js
+*/
+
+let reviewers = ["WillHa85", "GoldFry26", "Mittens41", "Tompkins8"];
+let reviewType = ["P", "N", "", ""]
+let stars = [5, 2, 1, 4];
+let reviewDates = ["11/18/2024", "11/17/2024", "11/15/2024", "11/10/2024"];
+let reviews = [
+   "I've owned all of the Dance Off games from the beginning. I have lost 6 pounds since I started playing.",
+   "A so-so release of this well-established game. Where this release fails is in the choreography. Many of the moves follow predictable patterns. I hope the next release improves the choreography .",
+   "The installation was buggy and kept crashing my gaming console. I spent several hours on tech support with no solution to my problem. I finally returned it and got my money back. I wish I could give it a zero star rating.",
+   "The latest version of Dance Off improves upon the 8th Edition only slightly; still is one of the best dance-style games on the market.",
+];
+let reviewTitles = ["My Favorite Workout Game", "Poor Choreography", "Buggy with Poor Tech Support", "Nice Improvement"];
+
